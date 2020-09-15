86c53ac39cc7fe42143f0e452a3e61c4a50daf18
joneff <joneff@gmail.com>
2020-09-15 09:52:03 +0300

    chore: add local preview for files

--- packages/font-icons/scripts/prepublish.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-// Lerna executes this script with the wrong cwd
-if ( process.cwd() !== path.resolve( __dirname, '../' ) ) {
-    console.log( 'Fixing process.cwd()' ); // eslint-disable-line no-console
-    process.chdir( path.resolve( __dirname, '../' ) );
-}
-
-require('./sass-build');
