05bc4f7d20d453189db254793435a71705a8d346
joneff <joneff@gmail.com>
2020-11-16 08:56:31 +0200

    chore: regen svg icon defintions

---
+++ packages/svg-icons/src/icons/table-position-end.ts
@@ -0,0 +1,7 @@
+import { SVGIcon } from '../svg-icon.interface';
+
+export const tablePositionEnd: SVGIcon = {
+    name: 'table-position-end',
+    content: '<path d="M32 384h448v32H32v-32zm0 96h320v-32H32v32zM480 32v224H256V32h224zM352 160h-64v64h64v-64zm0-96h-64v64h64V64zm96 96h-64v64h64v-64zm0-96h-64v64h64V64zM32 352h448v-32H32v32z" />',
+    viewBox: '0 0 512 512'
+}
