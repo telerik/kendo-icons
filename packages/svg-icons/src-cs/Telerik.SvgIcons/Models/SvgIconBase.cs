fd1d50833add433993bd891c128d9fca52bec1be
radkostanev <radkostanev@gmail.com>
2023-01-23 18:00:02 +0200

    feat: override svgicon equals to value comparison

--- packages/svg-icons/src-cs/Telerik.SvgIcons/Models/SvgIconBase.cs
+++ packages/svg-icons/src-cs/Telerik.SvgIcons/Models/SvgIconBase.cs
@@ -7,5 +7,22 @@ namespace Telerik.SvgIcons
         public string Content { get; set; }
 
         public string ViewBox { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as SvgIconBase;
+
+            if (obj == null)
+            {
+                return false;
+            }
+
+            return Name == other.Name && Content == other.Content && ViewBox == other.ViewBox;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name + Content + ViewBox).GetHashCode();
+        }
     }
 }
