cbbfcce885ed761ed85ba4f85de2f88a31883890
joneff <joneff@gmail.com>
2023-01-25 16:19:11 +0200

    fix: change border-style icon from using rect to path

--- packages/svg-icons/src/icons/border-style.ts
+++ packages/svg-icons/src/icons/border-style.ts
@@ -2,6 +2,6 @@ import { SVGIcon } from '../svg-icon.interface';
 
 export const borderStyleIcon: SVGIcon = {
     name: 'border-style',
-    content: '<path d="M32 96h32v320H32zM448 97h32v320h-32zM96 32h320v32H96zM96 448h320v32H96z" />',
+    content: '<path d="M32 96h32v320H32zm416 1h32v320h-32zM96 32h320v32H96zm0 416h320v32H96z" />',
     viewBox: '0 0 512 512'
 }
