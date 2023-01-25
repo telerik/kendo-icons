e373a427e5403d973f8e93b389a68d4fcf47749e
joneff <joneff@gmail.com>
2023-01-25 16:19:11 +0200

    fix: change border-style icon from using rect to path

--- packages/svg-icons/src-cs/Telerik.SvgIcons/Icons/BorderStyle.cs
+++ packages/svg-icons/src-cs/Telerik.SvgIcons/Icons/BorderStyle.cs
@@ -5,7 +5,7 @@ namespace Telerik.SvgIcons
         public BorderStyle()
         {
             Name = "border-style";
-            Content = "<path d=\"M32 96h32v320H32zM448 97h32v320h-32zM96 32h320v32H96zM96 448h320v32H96z\" />";
+            Content = "<path d=\"M32 96h32v320H32zm416 1h32v320h-32zM96 32h320v32H96zm0 416h320v32H96z\" />";
             ViewBox = "0 0 512 512";
         }
     }
